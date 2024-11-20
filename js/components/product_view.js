a81d1d5f2bffc658bba9f0e707c3446debaa7c27
HeinMinMinMaw <heinmin2maw.it@gmail.com>
2024-11-20 11:09:55 +0700

    fix: product's category in admin product list

--- js/components/product_view.js
+++ js/components/product_view.js
@@ -1,22 +1,22 @@
 import { sendAuthRequest } from "../api.js";
 import { fetchImageUrl } from "../utils.js";
-// Product Viewing
+ 
 document.addEventListener("DOMContentLoaded", async () => {
-    // Fetch and display products on DOM load
+    const tblBody = document.getElementById("product-tbl-body")
+   
     await DisplayProducts();
 
-    //Add event listener for delete buttons
-    document.querySelector("tbody").addEventListener("click", async (e) => {
+    tblBody.addEventListener("click", async (e) => {
         if (e.target.classList.contains("delete-btn")) {
             let productId = e.target.dataset.productId;
             await deleteProduct(productId);
         }
     });
-});
+
 
 async function DisplayProducts() {
-    let currentPage = 1; // Default to first page
-    let pageSize = 100; // Adjust based on your requirements
+    let currentPage = 1; 
+    let pageSize = 100;  
 
     let url = `/products/list?currentPage=${currentPage}&pageSize=${pageSize}`;
     let response = await sendAuthRequest(url, "GET", null);
@@ -30,19 +30,18 @@ async function DisplayProducts() {
 }
 
 async function populateProductTable(products) {
-    let productTableBody = document.querySelector(".product-table tbody");
-    productTableBody.innerHTML = ""; // Clear existing rows
+  
+    tblBody.innerHTML = ""; 
 
 
     products.forEach(async (product) => {
-        let imageUrl = `/products/images/${product.imageName}`;
         let row = document.createElement("tr");
         let objectUrl = await fetchImageUrl(product.imageUrl);
         row.innerHTML = `
             <td><img src="${objectUrl}" alt="Product Image" class="product-image"></td>
             <td>${product.name}</td>
             <td>${product.brand}</td>
-            <td>${product.categoryName || "Uncategorized"}</td>
+            <td>${product.category.name}</td>
             <td>$${product.price.toFixed(2)}</td>
             <td>
                 <span class="status ${product.stock > 0 ? "available" : "unavailable"}">
@@ -59,6 +58,8 @@ async function populateProductTable(products) {
                 <button class="delete-btn" data-product-id="${product.id}">Delete</button>
             </td>
         `;
-        productTableBody.appendChild(row);
+        tblBody.appendChild(row);
     });
 }
+
+});
